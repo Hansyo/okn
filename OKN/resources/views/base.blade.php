b733538f22f4b02abc6520e0e6fffc16c81a09f0
mon2929 <miagale55x39@gmail.com>
2020-05-09 14:21:21 -0700

    [add] ビューの追加

---
+++ OKN/resources/views/base.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title', 'OKN')</title>
+    <meta name="description" content="@yield('description')">
+
+    @section('style')
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @show
+</head>
+<body>
+    <header class="header">
+        <nav class="navi navi-main content-center">
+            <a href="{{ route('home') }}" class="navi-logo link-clear">
+                OKN  <!-- ロゴ -->
+            </a>
+            <div class="navi-main-right">
+                @section('navi-main-right')
+                @auth
+                <div>通知</div>
+                <div>ユーザ</div>
+                @endauth
+                @guest
+                <a href="{{ route('login') }}" class="link-clear">
+                    <div class="navi-btn btn-clear">ログイン</div>
+                </a>
+                <a href="{{ route('register') }}" class="link-clear">
+                    <div class="navi-btn btn-color">新規登録</div>
+                </a>
+                @endauth
+                @show
+            </div>
+        </nav>
+        <nav class="navi navi-sub content-center">
+            @section('navi-sub')
+            <div class="navi-sub-left">
+                @section('navi-sub-left')
+                <ul class="navi-menu">
+                    <li>ホーム</li>
+                    <li>メニュー</li>
+                    <li>メニュー</li>
+                    <li>メニュー</li>
+                </ul>
+                @show
+            </div>
+            <div class="navi-sub-right">
+                @section('navi-sub-right')
+                @show
+            </div>
+            @show
+        </nav>
+    </header>
+
+    <div class="body">
+        <div class="body-content content-center">
+            <div class="body-main">メイン</div>
+            <div class="body-sub">サブ</div>
+        </div>
+    </div>
+
+    <footer class="footer">
+        <div class="footer-content content-center">フッター</div>
+    </footer>
+</body>
+</html>
--- OKN/routes/web.php
+++ OKN/routes/web.php
@@ -15,4 +15,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('root');
+
+Route::get('/register', function () {
+    return view('auth.register');
+})->name('register');
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+Route::get('/home', function () {
+    return view('home');
+})->name('home');
