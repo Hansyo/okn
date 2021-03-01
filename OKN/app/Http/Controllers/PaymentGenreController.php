9fd0e04a6191cc052fe3ae015ba1aafefb5ee135
Hansyo <hansyo.souti.1999@gmail.com>
2021-03-02 01:10:08 +0900

    [Update] 不要な機能を削除 #11
    routing側で対処した機能があったので削除。

--- OKN/app/Http/Controllers/PaymentGenreController.php
+++ OKN/app/Http/Controllers/PaymentGenreController.php
@@ -8,11 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class PaymentGenreController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     /**
      * Display a listing of the resource.
      *
