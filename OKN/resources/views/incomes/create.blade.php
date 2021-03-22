d782e87173955dcf6cc97c5c950a25f1d4abd4c1
Hansyo <hansyo.souti.1999@gmail.com>
2021-03-22 16:50:30 +0900

    [Add] とりあえず収入のページ作成

---
+++ OKN/resources/views/incomes/create.blade.php
@@ -0,0 +1,24 @@
+@extends('home')
+@section('content')
+<div class="body-main">
+<form method="POST" action="{{ route('incomes.store') }}">
+    @csrf
+    <label>date</label><input id="date" type="date" name="date" /><br>
+    <label>amount</label><input id="amount" type="number" name="amount" /><br>
+    <label>memo</label><input id="memo" type="text" name="memo" /><br>
+    <label>genre</label><select id="incomeGenre" name="incomeGenre">
+        <option value="">必要なら以下から選択</option>
+        @foreach ($incomeGenres as $incomeGenre)
+        <option value="{{ $incomeGenre->id }}">{{ $incomeGenre->name }}</option>
+        @endforeach
+    </select><br>
+    <label>payment</label><select id="payment" name="payment">
+        @foreach ($payments as $payment)
+        <option value="{{ $payment->id }}">{{ $payment->name }}</option>
+        @endforeach
+    </select><br>
+    <p>支払い方法はあとから変更できません。もう一度確認してください。</p>
+    <button type="submit"> 登録 </button>
+</form>
+</div>
+@endsection
---
+++ OKN/resources/views/incomes/show.blade.php
@@ -0,0 +1,18 @@
+@extends('home')
+@section('content')
+<div class="body-main">
+    <table border="1">
+    <tr><td>date  </td><td>{{ $item->date }}  </td></tr>
+    <tr><td>amount  </td><td>{{ $item->amount }}  </td></tr>
+    <tr><td>genre  </td><td>@if($item->incomeGenre)<a href="{{ route('incomeGenres.show', $item->incomeGenre) }}">{{ $item->incomeGenre }}</a>@endif</td></tr>
+    <tr><td>payment  </td><td>@if($item->payment)<a href="{{ route('payments.show', $item->payment) }}">{{ $item->payment }}</a>@endif</td></tr>
+    <tr><td>memo  </td><td>{{ $item->memo }}</td></tr>
+</table>
+<button type="button" onclick="location.href='{{ route('incomes.edit', $item->id) }}'"> Edit </button>
+{{-- 削除用コード --}}
+<form id="delete" name="delete" method="POST" action="{{ route('incomes.destroy', $item->id) }}">
+@csrf
+@method('DELETE')
+<input type="button" onclick="((e)=>{if(window.confirm('本当に消す？')){document.delete.submit();}else{window.alert('キャンセルしました');}})()" value="削除">
+</div>
+@endsection
