f827b76b8c3c3f68f5adb982672a8bac35accd2d
Hansyo <hansyo.souti.1999@gmail.com>
2021-03-23 19:47:01 +0900

    [Fix] controllerとviewの変数名を統一

--- OKN/resources/views/genres/show.blade.php
+++ OKN/resources/views/genres/show.blade.php
@@ -2,18 +2,18 @@
 @section('main-section')
 <div class="body-main">
     <table border="1">
-    <tr><td>name  </td><td>{{ $genre->name }}  </td></tr>
-    <tr><td>memo  </td><td>{{ $genre->memo }}  </td></tr>
+    <tr><td>name  </td><td>{{ $item->name }}  </td></tr>
+    <tr><td>memo  </td><td>{{ $item->memo }}  </td></tr>
     <tr><td>parent</td><td>
-    @if($genre->parent)<a href="{{ route('genres.show', $genre->parent) }}">{{ $genre->parent }}</a>@endif
+    @if($item->parent)<a href="{{ route('genres.show', $item->parent) }}">{{ $item->parent }}</a>@endif
 </td></tr>
     <tr><td>child </td><td>@foreach ($childs as $child)
     <a href="{{ route('genres.show', $child) }}">{{ $child }}</a>
     @endforeach </td></tr>
 </table>
-<button type="button" onclick="location.href='{{ route('genres.edit', $genre->id) }}'"> Edit </button>
+<button type="button" onclick="location.href='{{ route('genres.edit', $item->id) }}'"> Edit </button>
 {{-- 削除用コード --}}
-<form id="delete" name="delete" method="POST" action="{{ route('genres.destroy', $genre->id) }}">
+<form id="delete" name="delete" method="POST" action="{{ route('genres.destroy', $item->id) }}">
 @csrf
 @method('DELETE')
 <input type="button" onclick="((e)=>{if(window.confirm('本当に消す？')){document.delete.submit();}else{window.alert('キャンセルしました');}})()" value="削除">
