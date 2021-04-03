0879d3875510da9a4636697e20c054956bf8cd57
mon2929 <miagale55x39@gmail.com>
2021-04-03 12:47:14 +0900

    [update] receiptsの各ビューを修正

--- OKN/resources/views/receipts/create.blade.php
+++ OKN/resources/views/receipts/create.blade.php
@@ -1,30 +1,36 @@
 @extends('base')
+
 @section('main-section')
-<div class="body-main">
-<form method="POST" action="{{ route('receipts.store') }}">
-    @csrf
-    <label>purchase</label><input id="name" type="date" name="purchase" /><br>
-    <label>amount</label><input id="name" type="number" name="amount" /><br>
-    <label>memo</label><input id="memo" type="text" name="memo" /><br>
-    <label>genre</label><select id="genre" name="genre">
-        @foreach ($genres as $genre)
-        <option value="{{ $genre->id }}">{{ $genre->name }}</option>
-        @endforeach
-    </select><br>
-    <label>store</label><select id="store" name="store">
-        <option value="">必要なら以下から選択</option>
-        @foreach ($stores as $store)
-        <option value="{{ $store->id }}">{{ $store->name }}</option>
-        @endforeach
-    </select><br>
-    <label>payment</label><select id="payment" name="payment">
-        <option value="">必要なら以下から選択</option>
-        @foreach ($payments as $payment)
-        <option value="{{ $payment->id }}">{{ $payment->name }}</option>
-        @endforeach
-    </select><br>
-    <p>支払い方法はあとから変更できません。もう一度確認してください。</p>
-    <button type="submit"> 登録 </button>
-</form>
-</div>
+<section class="bl-mainSection">
+    <h2 class="bl-mainSection-title">レシート作成</h2>
+    <div class="bl-mainSection-content bl-receiptCreateUnit">
+        <form method="POST" action="{{ route('receipts.store') }}">
+            @csrf
+            <fieldset class="bl-fieldset">
+                <label>purchase</label><input id="name" type="date" name="purchase">
+                <label>amount</label><input id="name" type="number" name="amount">
+                <label>memo</label><input id="memo" type="text" name="memo">
+                <label>genre</label><select id="genre" name="genre">
+                    @foreach($genres as $genre)
+                    <option value="{{ $genre->id }}">{{ $genre->name }}</option>
+                    @endforeach
+                </select>
+                <label>store</label><select id="store" name="store">
+                    <option value="">必要なら以下から選択</option>
+                    @foreach($stores as $store)
+                    <option value="{{ $store->id }}">{{ $store->name }}</option>
+                    @endforeach
+                </select>
+                <label>payment</label><select id="payment" name="payment">
+                    <option value="">必要なら以下から選択</option>
+                    @foreach($payments as $payment)
+                    <option value="{{ $payment->id }}">{{ $payment->name }}</option>
+                    @endforeach
+                </select>
+            </fieldset>
+            <p>支払い方法はあとから変更できません。もう一度確認してください。</p>
+            <button class="el-btn" type="submit">登録</button>
+        </form>
+    </div>
+</section>
 @endsection
--- OKN/resources/views/receipts/index.blade.php
+++ OKN/resources/views/receipts/index.blade.php
@@ -1,15 +1,24 @@
 @extends('base')
+
 @section('main-section')
-<div class="body-main">
-    <table border="1">
-    <tr><th>買い物日</th><th>ジャンル</th><th>メモ</th></tr>
-    @foreach ($items as $receipt)
-    <tr>
-<td><a href="{{route('receipts.show', $receipt->id)}}">{{ $receipt->purchase }}</a></td>
-<td><a href="{{route('genres.show', $receipt->genre)}}">{{ $receipt->genre }}</a></td>
-<td>{{ $receipt->memo }}</td></tr>
-    @endforeach
-</table>
-<button type="button" onclick="location.href='{{ route('receipts.create') }}'">新規追加</button>
-</div>
+<section class="bl-mainSection">
+    <h2 class="bl-mainSection-title">レシート一覧</h2>
+    <div class="bl-mainSection-content bl-receiptUnit">
+        <table class="bl-table">
+            <thead>
+                <tr><th>買い物日</th><th>ジャンル</th><th>メモ</th></tr>
+            </thead>
+            <tbody>
+                @foreach ($items as $receipt)
+                <tr>
+                <td><a href="{{ route('receipts.show', $receipt->id) }}">{{ $receipt->purchase }}</a></td>
+                <td><a href="{{ route('genres.show', $receipt->genre) }}">{{ $receipt->genre }}</a></td>
+                <td>{{ $receipt->memo }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <button class="el-btn" type="button"><a href="{{ route('receipts.create') }}">新規作成</a></button>
+    </div>
+</section>
 @endsection
