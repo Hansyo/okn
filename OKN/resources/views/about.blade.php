7cd1c67cf4294ffb9f43a3fa3bf9f697e870da9d
Hansyo <hansyo.souti.1999@gmail.com>
2021-04-11 15:24:44 +0900

    [Add] aboutページを雑に作成
    @Hansyo に文才がないことがここに証明された

---
+++ OKN/resources/views/about.blade.php
@@ -0,0 +1,25 @@
+@extends('base')
+
+@section('main-section')
+<section class="bl-mainSection bl-mainSection--colorMain">
+    <h1>私達はオカンのような存在を目指しています</h1>
+</section>
+<section class="bl-mainSection">
+    <p>OKN:Kakeibo Nyumon は大学生が使いやすい家計簿がほしいという声を受けて生まれたシンプルでわかりやすい家計簿です。</p>
+    <p>その名前の通り、家計簿をまだ使ったことのない一人暮らしの大学生をターゲットとし、家計簿の習慣をつけてもらうことを目的としています。</p>
+    <p>そのため、家計簿の習慣がついたと思えば、他のサービスに転向することをおすすめします。OKNには便利な機能はあんまりありませんので...</p>
+</section>
+<section class="bl-mainSection bl-mainSection--colorMain">
+    <h2>OKNの目玉機能：プリセット</h2>
+</section>
+<section class="bl-mainSection">
+    <p>OKNの目玉機能はプリセット機能です。学食や美容院などの、金額が変わらない支払い等をかんたんにレシートとして登録することができます。</p>
+</section>
+<section class="bl-mainSection bl-mainSection--colorMain">
+    <h2>開発者</h2>
+</section>
+<section class="bl-mainSection">
+    <p>判匠五気(<a href="https://github.com/Hansyo">github</a>)</p>
+    <p>mon2929(<a href="https://github.com/mon2929">github</a>)</p>
+</section>
+@endsection
